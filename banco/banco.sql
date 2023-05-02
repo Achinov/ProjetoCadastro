0a32662067c1acaafd4aa4c98655273853e5a184
Achinov <gabrielarquilinoa.a@gmail.com>
2023-05-02 11:33:31 -0300

    RATINHO

--- banco/banco.sql
+++ banco/banco.sql
@@ -6,8 +6,20 @@ USE projeto1;
 
 CREATE TABLE usuario(
 id_usuario int(4) PRIMARY KEY auto_increment,
-nome_usario varchar(50) NOT NULL,
+nome_usuario varchar(50) NOT NULL,
 email_usuario varchar(50) NOT NULL,
 fone_usuario varchar(30) NULL,
 senha		 varchar(30) NOT NULL
+
+
+
+/**
+use  projeto1;
+-- INSERIR DADOS
+INSERT INTO usuario
+(nome_usuario, fone_usuario, email_usuario, senha)
+VALUES ('CÁSSIO', '(18) 3622-9046', 'cassio#mail.com', '1234');
+
+select * from usuario;
+**/
 );
