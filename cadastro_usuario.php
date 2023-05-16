00eab8dbf746c24bd5aba1cd1c3d7997d36efed8
Achinov <gabrielarquilinoa.a@gmail.com>
2023-05-16 12:02:26 -0300

    caua

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -11,8 +11,20 @@
         echo "Telefone: $fone <br>";
         echo "Senha: $senha <br>";
 
+        $compara= "SELECT * FROM usuario WHERE email_usuario = '$email'";
+        $result = mysqli_query($con,$compara);
+        $row = mysqli_num_rows($result);
+      
+        if($row>0){
+                        echo "<h1>Usuário já cadastrado!</h1>";
+                        echo "<a href='cadastro_usuario.html'>Voltar</a>";
+                        exit();               
+        }else{
+
         $sql = "INSERT INTO usuario (nome_usuario, email_usuario,
                                         fone_usuario, senha)";
+
+
         $sql .= "VALUES ('".$nome."','".$email."','".
                             $fone."','".$senha."')";
         
@@ -23,8 +35,7 @@
         echo "Dados cadastrados com sucesso!";
         else
         echo"Erro ao tentar cadastrar!";
-                        
-
+        }
 
 
 ?>
